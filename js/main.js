1a32bb15496c329233a31a3495c49595151e10b4
Oorzhak Alexander <oorzhak@inj-geo.local>
2020-11-08 02:02:25 +0300

    add section program

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
 $(function() {
-  $('.menu__btn').on('click', function() {
-    $('.menu__list').slideToggle();
-  })
+    $('.menu__btn').on('click', function() {
+        $('.menu__list').slideToggle();
+    })
 })
+
+$(".program-slider").slick({
+            arrows: true,
+            dots: true,
+            dotsClass: "program-dots",
+            responsive: [{
+                    breakpoint: 516,
+                    settings: {
+                        arrows: false,
+                    }
+                }
+            })
