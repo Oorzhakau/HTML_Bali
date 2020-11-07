5296cc46ea772437fca8ea4079778655f95267b7
Oorzhak Alexander <oorzhak@inj-geo.local>
2020-11-08 02:15:25 +0300

    fixed error on slick

--- js/main.js
+++ js/main.js
@@ -2,16 +2,16 @@ $(function() {
     $('.menu__btn').on('click', function() {
         $('.menu__list').slideToggle();
     })
-})
 
-$(".program-slider").slick({
-            arrows: true,
-            dots: true,
-            dotsClass: "program-dots",
-            responsive: [{
-                    breakpoint: 516,
-                    settings: {
-                        arrows: false,
-                    }
-                }
-            })
+    $(".program-slider").slick({
+        arrows: true,
+        dots: true,
+        dotsClass: "program-dots",
+        responsive: [{
+            breakpoint: 516,
+            settings: {
+                arrows: false,
+            }
+        }]
+    })
+})
